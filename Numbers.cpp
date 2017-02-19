9ddff1ec4ad8632799c1a1db205c80d0945b5ccb
Wedrig <jackkmorice@gmail.com>
2017-02-19 11:28:18 -0600

    Add files via upload

---
+++ Numbers.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include "string"
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	
+	string command;
+	while (1 == 1)
+	{
+		command.clear();
+		cout << "What's your name again? I appreciate you coming here so late.";
+		getline(cin, command);
+		cout << "Thanks " << command;
+		cin.get()
+			; cout << "\nRemember, when using the Va11, never put in numbers, for some reason i't wont work, so remember..\n";
+		cin.get()
+			; cout << "Numbers aren't cool\n";
+			; cout << "A C++ Game Made By Wedrig\n";
+		; cout << "Press Enter to start the game." << endl;
+		cin.get()
+			; string pass1 = "1"; \
+			; string pass2 = "1738"; \
+			; string pass3 = "27"; \
+			; string yes = "yes"; \
+			; string no = "no"; \
+
+		string input;
+		cout << "ENTER PASSWORD\n ";
+		cout << "HINT: YOU ARE NUMBER "
+			; cin >> input;
+		if (input == pass1) {
+			cout << "WE ARE NUMBER ONE\n";
+			cin.get()
+				; cout << "CORRECT. PLEASE CONFIRM YOUR IDENTITY";
+			 cin.get()
+				 ; cout << "HINT: KYS IF YOU KNOW THIS ONE\n";
+			 ; cout << "ENTER PASSWORD\n ";
+		 
+			 ; cin >> input;
+			if (input == pass2) {
+				cout << "KYS\n";
+				cin.get()
+					; cout << "CORRECT YET AGAIN. KEEP CONFIRMING\n";
+				cin.get()
+					; cout << "BEFORE WE CONTINUE. I HAVE A ADVRETISEMENT TO SHOW YOU, IN ORDER TO GAIN MONEY";
+				; cout << "What are you guys playing? I’m supposed to be playing Game Of War™ but this one player keeps kicking my donkey. Is it TheLegend27?! Yeah, TheLegend27. Who is the legend 27?\n";
+				; cout << " Some say TheLegend27 is the first Game Of War™ player ever.Born from fire.I heard, TheLegend27 can hurl a boulder farther than a catapult.I heard TheLegend27 once defeated an entire army with a single blow.But the worst part is, just when you think you’re safe, just when you think you might escape… WHAM!!!Just like that!collective gasps Iphone Notification What ? No, no no no, no no no!Iphone Notification That’s impossible!“TheLegend27” has captured my hero!Mine too!Aw you guys, I’m sure it’s not that bad.You have no idea what just happened.I might have some idea.Woman turns around, and powers her device’s screen, showing she is in fact TheLegend27\n";
+				cin.get()
+					; cout << "THANK YOU FOR YOUR PATIENCE, BACK TO CRACKING!"
+					; cin.get()
+				; cout << "ENTER PASSWORD\n";
+				; cin >> input;
+				if (input == pass3) {
+					cout << "ARE YOU THE LEGEND27?\n"
+						; cin.get()
+						; cout << "YOU HAVE 1 NEW FILE ACCESSABLE"
+						; cin.get()
+						; cout << "READ?";
+					; cin >> input;
+					if (input == yes) {
+						cout << "'According to all known laws of aviation, a bee should not be able to fly, it's wings are too small to get it's little fat body off the ground-\n'";
+						cout << "Looks like the rest is locked. Should check in on that later";
+						if (input == no) {
+							cout << "LET US CONTINUE ON THEN";
+
+					}
+					}
+				}
+
+		}
+		}
+		else {
+			cout << "COMMAND INCORRECT\n";
+			cin.get()
+				; cout << "PLEASE RE-ENTER THE CORRECT CODE"
+				; cin.get()
+				;
+
+		}
+		return 0;
+	}
+}
